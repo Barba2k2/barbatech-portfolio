6cbf25e4162527f57ba0a2352a8ec1e7bf2d1f20
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-01-23 15:02:10 -0300

    feat(components): Created initial navbar component with logo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <div>
+      <Image src="/logo.png" alt="Logo" width={50} height={50} />
+    </div>
+  );
+}
