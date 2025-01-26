d693bc5baa29c8240b88c5be90f9e5870aba2052
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-01-25 21:00:43 -0300

    feat: Updates Tailwind config with dark mode and new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,51 +10,99 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        blackText: "#111111",
         button: {
-          primary: "#CC06FF",
+          primary: "#D300FF",
           hover: "#9507e3",
           secondary: "#FFFFFF",
         },
-        primary: "#C02648",
-        red: {
-          backgorung15: "rgba(213, 110, 132, 0.15)",
-          300: "#D56E84",
-          600: "#AF2342",
-          700: "#881B33",
-          card: "rgba(136, 27, 51, 0.7)",
-          accordion: "rgba(192, 38, 72, 0.05)",
+        logo: {
+          blue: "#00BAFF",
+          purple: "#D300FF",
         },
         black: {
+          "20": "rgba(0, 0, 0, 0.2)",
+          "64": "rgba(0, 0, 0, 0.64)",
+          "99": "rgba(17, 17, 17, 0.99)",
           bg: "#222222",
           secondary: "#141414",
           tertiary: "#212429",
-          20: "rgba(0, 0, 0, 0.2)",
-          64: "rgba(0, 0, 0, 0.64)",
-          99: "rgba(17, 17, 17, 0.99)",
         },
         gray: {
-          100: "#F8F9FA",
-          300: "#DDE2E5",
-          600: "#495057",
+          "100": "#F8F9FA",
+          "300": "#DDE2E5",
+          "600": "#495057",
           secondary: "#7E7E7E",
           mid: "#495057",
           tertiary: "#ACB5BD",
           support: "#F8F9FA",
-          background: "rgba(255, 255, 255, 0.08)",
+          background: "rgba(255, 255, 255, 0.02)",
           background04: "rgba(255, 255, 255, 0.04)",
         },
         white: {
+          "100": "#ffffff0a",
           primary: "#FFFFFF",
-          100: "#ffffff0a",
         },
         green: {
+          "50": "rgba(38, 192, 138, 0.5)",
           checkmark: "#26C08A",
-          50: "rgba(38, 192, 138, 0.5)",
         },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      screens: {
+        desktop: "1440px",
+        tabletLarge: "1024px",
+        tablet: "744px",
+        mobile: "380px",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
